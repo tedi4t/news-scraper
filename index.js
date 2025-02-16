ca6257954c2718e917be28d5f2695961dbe6c5f8
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2025-02-16 20:29:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ async function getNewsHeadlines(url) {
     }
 }
 
-
-
-
-
 (async () => {
     const newsUrl = "https://www.bbc.com/news"; // Change this to any news website
     const headlines = await getNewsHeadlines(newsUrl);
